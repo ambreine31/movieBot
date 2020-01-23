5d213a0054fc8dc00ddbf22808a47d9427429431
ambreine31 <bourdierambre@gmail.com>
2020-01-23 09:08:06 +0100

    add server

---
+++ server.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const express = require('express');
+
+const server = express();
+const PORT = process.env.PORT || 3000;
+
+server.get('/', (req,res) => res.send("hello!!"));
+server.listen(PORT, ()=>console.log('the bot server is running on port ${PORT}'));
