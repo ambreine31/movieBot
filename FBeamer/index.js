a4451a08ae8c5bd2639bb659dd97a814528161e2
ambreine31 <bourdierambre@gmail.com>
2020-02-05 14:51:31 +0100

    td 4

--- FBeamer/index.js
+++ FBeamer/index.js
@@ -1,4 +1,5 @@
 'use strict'
+const bodyParser = require("body-Parser");
 
 class FBeamer{
   constructor({pageAccessToken, VerifyToken}){
@@ -23,6 +24,19 @@ class FBeamer{
       console.log(e);
     }
   }
+
+  incoming(req, res, cb){
+    let data = req.body;
+    console.log("incoming",data);
+    if(data.object === 'page'){
+      data.entry.forEach(pageObj => {
+        console.log(pageObj.messaging);
+      });
+    }
+    res.sendStatus(200);
+  }
+
+  //message(id,time, )
 }
 
 module.exports = FBeamer;
--- server.js
+++ server.js
@@ -2,12 +2,17 @@
 
 const express = require('express');
 const conf = require('./config');
+const bodyParser = require("body-Parser");
 const FBeamer = require('./FBeamer');
 
 const server = express();
+server.use(bodyParser.json());
 const PORT = process.env.PORT || 3000;
 
 const fb = new FBeamer(conf.FB);
 
 server.get('/', (req,res) => fb.registerHook(req,res));
-server.listen(PORT, ()=>console.log('the bot server is running on port ${PORT}'));
+server.post('/', (req,res) => fb.incoming(req,res));
+server.listen(PORT, ()=>console.log(`the bot server is running on port ${PORT}`));
+console.log(res.nlp.entities)
+//ngrok http 3000
